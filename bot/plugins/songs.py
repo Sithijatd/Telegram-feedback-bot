693544fadf9ea9e3057c36073e469dd4902acc96
Sithija_td <103871031+Sithijatd@users.noreply.github.com>
2022-07-25 06:57:53 +0530

    Create songs.py

---
+++ bot/plugins/songs.py
@@ -0,0 +1,74 @@
+import os
+from bot import app as bot
+from pyrogram import idle, filters
+import requests
+import yt_dlp as youtube_dl
+from pyrogram import filters, Client
+from youtube_search import YoutubeSearch
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
+
+@bot.on_message(filters.command('song') & ~filters.forwarded)
+def song(client, message):
+
+    user_id = message.from_user.id 
+    user_name = message.from_user.first_name 
+    rpk = "["+user_name+"](tg://user?id="+str(user_id)+")"
+
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    m = message.reply("🔎 Searching...")
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        #print(results)
+        title = results[0]["title"][:40]       
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f'thumb{title}.jpg'
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, 'wb').write(thumb.content)
+        
+        performer = f"〢ImSithijaBot〣"  
+        duration = results[0]["duration"]
+        url_suffix = results[0]["url_suffix"]
+        views = results[0]["views"]
+
+    except Exception as e:
+        m.edit(
+            "❌ Cannot find song use another keywords"
+        )
+        print(str(e))
+        return
+    m.edit("📥 Downloading...")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = (f"""
+🏷 **Title:** [{title}]({link})
+⏳ **Duration:** `{duration}`
+👀 **Views:** `{views}` 
+👤**Requested By**: {message.from_user.mention()}
+📤 **Uploaded By: [❦Iᴛ'ꜱ Mᴇ Sɪᴛʜɪᴊᴀ❦](https://t.me/ItsMeSithija)**
+        """)
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        s = message.reply_audio(audio_file, caption=rep, performer=performer, thumb=thumb_name, title=title, duration=dur)
+        m.delete()
+    except Exception as e:
+        m.edit('❌ Error occurred.')
+        print(e)
+
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
